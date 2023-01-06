5efa2f219425443806743006d3e445e699df95f9
Frederic Laing <frederic.laing.development@gmail.com>
2023-01-06 13:22:03 +0100

    format cmake files

---
+++ .cmake-format.py
@@ -0,0 +1,7 @@
+# -----------------------------
+# Options effecting formatting.
+# -----------------------------
+with section("format"):
+
+  # List files one by one for commands like "add_executable"
+  max_pargs_hwrap = 4

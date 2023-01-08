8eb82c4bd5242c2908a14f74e65805f9931cff12
Frederic Laing <frederic.laing.development@gmail.com>
2023-01-08 22:22:52 +0100

    ddk: use cxxopts for command line argument parsing

--- src/ddk/ddk.cpp
+++ src/ddk/ddk.cpp
@@ -1,67 +1,13 @@
 #include "fsinfo_parser.hpp"
-#include "input_params_parser.hpp"
 #include "version.h"
 #include <algorithm>
+#include <cxxopts.hpp>
 #include <fmt/core.h>
-
-static void printHelpOptions() {
-    fmt::print(fmt::format(
-        R"(DeDuplicationKit (ddk) is a simple command line tool for finding duplicate files at high speed.
-
-Usage: ddk [OPTION]...
-
-Options:
--h, --help      display this manual and exit
-
--v, --version   show version
-
--p PATH         search for duplicates in PATH (default: current working
-                directory)
-
--f PATH         search for duplicates of files passed by "-f PATH" in 
-                "-p PATH"
-
--d              show detailed information about deduplication scan
-
--l              follow symbolic links during deduplication scan
-                (default: deactivated)‚
-
--r              remove duplicates (PERMANENTLY DELETES FILES! USE WITH
-                CAUTION!)
-
--ri             go through each duplicate one by one and let the user
-                select which files should be deleted
-
-Examples:
-ddk
-ddk -p "~/Documents" -l
-ddk -p "/path/to/files/" -d
-ddk -f "/path/to/file1.txt"
-ddk -p "./path/to/files/" -f "./path/to/files/file1.txt"
-ddk -p "./path/to/files/" -r
-ddk -p "./path/to/files/" -ri
-)"));
-}
 
 static void printVersion() { fmt::print("{}\n", DDK_VERSION); }
 
-static bool getPath(const InputParser *const input, std::filesystem::path &selectedPath) {
-    if (!input->cmdOptionExists("-p")) {
-        selectedPath = std::filesystem::current_path();
-        return true;
-    }
-    std::string path = input->getCmdOption("-p");
-    if (path.empty()) {
-        return false;
-    }
-    const auto tilde = path.find('~');
-    if (tilde != std::string::npos) {
-        const std::string home = std::getenv("HOME");
-        path.replace(tilde, 1, home);
-    }
-    selectedPath = std::filesystem::path(path);
-
-    return true;
+static void printInvalidOptions() {
+    fmt::print("ERROR: Illegal options. Use \"ddk -h\" for help.\n");
 }
 
 static void printResultsDedup(const DDK::FileSystemInfo *const fsinfo) {
@@ -73,33 +19,70 @@ static void printResultsDedup(const DDK::FileSystemInfo *const fsinfo) {
 }
 
 int main(int argc, char *argv[]) {
-    const InputParser input(argc, argv);
+    cxxopts::Options options(
+        "ddk", "DeDuplicationKit (ddk) is a simple command line tool for finding duplicate files.");
 
-    if (input.cmdOptionExists("-h") || input.cmdOptionExists("--help")) {
-        printHelpOptions();
+    // clang-format off
+    options.add_options()
+        ("h,help", "Display this manual and exit", cxxopts::value<bool>()->default_value("false"))
+        ("v,version", "Display semantic version of ddk", cxxopts::value<bool>()->default_value("false"))
+        ("p,path", "Search for duplicates in specific path \"arg\"", cxxopts::value<std::string>())
+        ("c,compare", "Compare content of path \"-p arg\" with content of path \"-c arg\" and lists all duplicates found in path \"-p arg\"", cxxopts::value<std::string>())
+        ("d,detailed", "Show detailed information about deduplication scan", cxxopts::value<bool>()->default_value("false"))
+        ("l,symlinks", "Follow symbolic links during deduplication scan", cxxopts::value<bool>()->default_value("false"))
+        ("r,remove", "Remove duplicates (PERMANENTLY DELETES FILES! USE WITH CAUTION!)", cxxopts::value<bool>()->default_value("false"))
+        ("i,remove-interactive", "Go through each duplicate one by one and let the user select which files should be deleted", cxxopts::value<bool>()->default_value("false"))
+        ;
+    // clang-format on
+
+    cxxopts::ParseResult result;
+
+    try {
+        result = options.parse(argc, argv);
+    } catch (const std::exception &e) {
+        printInvalidOptions();
+        return 1;
+    }
+
+    if (result["h"].as<bool>()) {
+        fmt::print(options.help());
         return 0;
     }
 
-    if (input.cmdOptionExists("-v") || input.cmdOptionExists("--version")) {
+    if (result["v"].as<bool>()) {
         printVersion();
         return 0;
     }
 
-    std::filesystem::path path;
-    if (!getPath(&input, path)) {
-        fmt::print("ERROR: Custom path was choosen (\"-p\"), but no path was providen.\n");
-        return 1;
+    const bool analyze_symLinks = result["l"].as<bool>();
+    const bool detailed = result["d"].as<bool>();
+    const bool remove = result["r"].as<bool>();
+    const bool remove_interactive = result["i"].as<bool>();
+    std::filesystem::path sanitized_path;
+
+    if (result.count("p") == 0) {
+        sanitized_path = std::filesystem::current_path().string();
     }
-    if (!std::filesystem::exists(path)) {
-        fmt::print("ERROR: Custom path {} does not exists.\n", path.string());
-        return 1;
+    if (result.count("p") == 1) {
+        std::string path = result["p"].as<std::string>();
+
+        const auto tilde = path.find('~');
+        if (tilde != std::string::npos) {
+            const std::string home = std::getenv("HOME");
+            sanitized_path = path.replace(tilde, 1, home);
+        }
+    } else {
+        fmt::print("ERROR: Multiple paths selected with option \"-p\"\n");
+        printInvalidOptions();
     }
 
-    // read general parameters
-    const bool analyzeSymLinks = input.cmdOptionExists("--symlinks");
+    if (!std::filesystem::exists(sanitized_path)) {
+        fmt::print("ERROR: Custom path {} does not exists.\n", sanitized_path.string());
+        return 1;
+    }
 
     // Scan file system structure
-    const DDK::FileSystemInfo fsinfo(path, analyzeSymLinks);
+    const DDK::FileSystemInfo fsinfo(std::filesystem::path(sanitized_path), analyze_symLinks);
 
     printResultsDedup(&fsinfo);
 
